f7187471450ee45eda7ddf7053806e679059d0b7
malorisdead <malorisdead@gmail.com>
2021-01-11 22:52:44 -0800

    Reduce and make configurable the passive factoid listener

--- src/Gambot.Module.Factoid/FactoidListener.cs
+++ src/Gambot.Module.Factoid/FactoidListener.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Gambot.Core;
@@ -7,21 +8,33 @@ namespace Gambot.Module.Factoid
 {
     public class FactoidListener : IListener
     {
+        private readonly IConfig _config;
+        private readonly Random _random;
         private readonly IDataStoreProvider _dataStoreProvider;
+        private readonly ILogger _log;
 
-        public FactoidListener(IDataStoreProvider dataStoreProvider)
+        public FactoidListener(IDataStoreProvider dataStoreProvider, IConfig config, ILogger log)
         {
+            _log = log;
+            _config = config;
             _dataStoreProvider = dataStoreProvider;
+            _random = new Random();
         }
 
         public async Task Listen(Message message)
         {
-            var match = Regex.Match(message.Text, @"^(.{0,42})\s+(is(?: also)?|are)\s+(.+)", RegexOptions.IgnoreCase);
+            var chance = Int32.Parse(await _config.Get("PercentChanceOfPassiveFactoid", "10"));
+            if (_random.Next(100) > chance)
+                return;
+
+            var match = Regex.Match(message.Text, @"^(.{0,42})\s+(is(?: also)?|are)\s+([^?]+)", RegexOptions.IgnoreCase);
             if (!match.Success)
                 return;
-            
+
+            _log.Debug("Someone said a factoid!");
+
             var dataStore = await _dataStoreProvider.GetDataStore("Factoids");
-            
+
             var trigger = match.Groups[1].Value;
             var verb = match.Groups[2].Value;
             var tidbit = match.Groups[3].Value;
